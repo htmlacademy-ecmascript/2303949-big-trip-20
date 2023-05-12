aa45c0eac8ea2c18bdfa559fae727dbdaf794b06
Ivan <ri_ch@bk.ru>
2023-05-12 16:18:55 +0300

    перенес рендер из main.js в events-presenter.js

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -3,7 +3,10 @@ import {render} from '../render';
 import TripEventsListView from '../view/events-list-view';
 import TripEventEditView from '../view/event-edit-view';
 import TripEventView from '../view/event-view';
+import TripFiltersView from './view/filters-view.js';
 
+const pageHeader = document.querySelector('.page-header');
+const tripControlsFiltersElement = pageHeader.querySelector('.trip-controls__filters');
 
 export default class TripEventsPresenter {
   tripSortComponent = new TripSortView();
@@ -18,7 +21,7 @@ export default class TripEventsPresenter {
     render(this.tripEventsComponent, this.container);
 
     render(new TripEventEditView(), this.tripEventsComponent.getElement());
-
+    render(new TripFiltersView(), tripControlsFiltersElement);
     const tempTripEventsStubsCount = 3;
 
     for (let i = 0; i < tempTripEventsStubsCount; i++) {
