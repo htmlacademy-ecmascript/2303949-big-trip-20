3c15f68d5ce625631192d5258eabd2781d1da28d
Ivan <ri_ch@bk.ru>
2023-05-12 16:22:08 +0300

    ok

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-import TripFiltersView from './view/filters-view.js';
-import TripEventsPresenter from './presenter/events-presenter';
-import {render} from './render.js';
 
-const pageHeader = document.querySelector('.page-header');
-const tripControlsFiltersElement = pageHeader.querySelector('.trip-controls__filters');
+import TripEventsPresenter from './presenter/events-presenter';
+
 
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsElement = siteMainElement.querySelector('.trip-events');
 const boardPresenter = new TripEventsPresenter({container: tripEventsElement});
 
-render(new TripFiltersView(), tripControlsFiltersElement);
 
 boardPresenter.init();
